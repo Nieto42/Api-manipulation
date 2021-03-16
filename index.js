469ab7df5419dd470290663e26e70e338084e508
Neto Pompeu <neto1@hotmail.fr>
2021-03-16 15:08:49 +0100

    try again api quote + img

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-const quote = document.querySelector('.quote');
+const quote = document.querySelector('.quote')
 
-const getQuote =  () => {
+const getQuote = () => {
 
     fetch('https://api.quotable.io/random')
     .then((res) => res.json())
     .then((data) => {
         quote.innerHTML = data.content
-        console.log(data);
+        console.log(data.content);
     })
 
-    fetch('https://picsum.photos/1600/1000')
-    .then((res) => { 
-        document.querySelector('.pic').innerHTML = 
-        `<img src=${res.url} />`
+    fetch('https://picsum.photos/400/400')
+    .then((res) => {
+        document.querySelector('.pic').innerHTML = `<img src=${res.url} />`
         console.log(res.url)
-
     })
-    
 }
+    
 quote.addEventListener('click', () => {
     getQuote()
 })
-
